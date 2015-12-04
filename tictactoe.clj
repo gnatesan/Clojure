5d50776d1f4a1763af83700e647369d0d640e997
Erik <eponder@mymail.mines.edu>
2015-12-04 14:45:36 -0700

    Added logic for recieveing moves

--- tictactoe.clj
+++ tictactoe.clj
@@ -18,11 +18,20 @@
   [board player x y]
     (assoc board y (assoc (get board y) x player)))
 
+(defn get-move [board]
+	(println "Pleaser enter an x and y: ")
+	(let[x (read-string (read-line)) y (read-string (read-line))]
+	(if (and (< x (count board)) (>= x 0 ) (< y (count board)) (>= y 0 ) (= (nth (nth board x) y) \space)) {:x x, :y y} (do (println "Please Enter a Valid Move: ") (get-move board)))
+	))
+ 
 (def board (gen-board 10))
 (def board (make-move board \X 2 2))
 (def board (make-move board \O 4 0))
 (def board (make-move board \X 1 1))
 (print-board board)
+
+
+
 
 
 ; general structure:
