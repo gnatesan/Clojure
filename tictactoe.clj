ffbb880406fec07856f8a95992c9b99cfba270a1
Zack Webb <ztwebb42@gmail.com>
2015-12-04 19:51:17 -0700

    Added random computer player.

--- tictactoe.clj
+++ tictactoe.clj
@@ -17,14 +17,20 @@
 
 (defn make-move
   "Return an altered board state with a move made at the given coordinate."
-  [board player x y]
+  [board player {x :x y :y}]
     (assoc board y (assoc (get board y) x player)))
 
 (defn get-move [board]
-	(println "Pleaser enter an x and y: ")
+	(println "Please enter an x and y: ")
 	(let[x (read-string (read-line)) y (read-string (read-line))]
-	(if (and (< x (count board)) (>= x 0 ) (< y (count board)) (>= y 0 ) (= (nth (nth board x) y) \space)) {:x x, :y y} (do (println "Please Enter a Valid Move: ") (get-move board)))
+	(if (and (< x (count board)) (>= x 0 ) (< y (count board)) (>= y 0 ) (= (nth (nth board y) x) \space)) {:x x, :y y} (do (println "Please Enter a Valid Move: ") (get-move board)))
 	))
+  
+(defn comp-move 
+  [board]
+  (rand-nth
+    (for [x (range 0 (count board)) y (range 0 (count board))
+      :when (= (nth (nth board y) x) \space)] {:x x :y y})))
 
 (defn check-win
   [board]
@@ -32,23 +38,16 @@
         
 (defn main-loop
   "The game loop."
-  [board moves who]
-  (cond
-    (check-win board) (if (= who \X) \O \X)
-    (>= moves (* (count board) (count board))) (do (print-board board) "Stalemate")
-    :else (do (print-board board)
-      (if (= who \X)
-        (main-loop (make-move board \X (mod moves (count board)) (quot moves (count board))) (inc moves) \O)
-        (main-loop (make-move board \O (mod moves (count board)) (quot moves (count board))) (inc moves) \X)))))
+  ([board moves who]
+    (cond
+      (check-win board) (if (= who \X) "Computer wins." "Player wins!")
+      (>= moves (* (count board) (count board))) (do (print-board board) "Stalemate.")
+      :else (do (print-board board) (println)
+        (if (= who \X)
+          (main-loop (make-move board \X (get-move board)) (inc moves) \O)
+          (main-loop (let [t (comp-move board)] (println t) (make-move board \O t)) (inc moves) \X)))))
+  ([board] (main-loop board 0 \X))
+  ([] (main-loop (gen-board) 0 \X)))
 
-; general structure:
-; 1. ask for board size
-; 2. generate empty board
-; 3. loop:
-;   1. print board
-;   2. player's turn
-;   3. check for win
-;   4. print board
-;   5. computer's turn
-;   6. check for win
-; 4. print a message
+(def test-board [[\X \space \X \X] [\space \X \O \space] [\space \space \O \space] [\space \O \O \space]])
+
